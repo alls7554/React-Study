885c9069139a2e54dbe371ca7ec5dfcdafe76b18
alls7554 <alls970459@gmail.com>
2021-07-20 18:00:17 +0900

    modify Counter.js

--- 3.component-tutorial/src/Counter.js
+++ 3.component-tutorial/src/Counter.js
@@ -29,20 +29,53 @@
 
 // export default Counter;
 
+
+// state 객체 안에 여러 값이 있을 때
+// import React, { Component } from 'react';
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
+//     // state의 default value 설정하기
+//     this.state = {
+//       number: 0,
+//       fixedNumber: 0
+//     };
+//   }
+//   render() {
+// 		// state를 조회할 때는 this.state로 조회합니다.
+//     const { number, fixedNumber } = this.state;
+//     return (
+//       <div>
+//         <h1>{number}</h1>
+//         <h2>바뀌지 않는 값: {fixedNumber}</h2>
+//         <button
+//           // onClick을 통해 버튼이 클릭되었을 때, 호출할 함수 지정
+//           onClick={() => {
+//             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+//             this.setState({ number:number+1 })
+//           }}
+//         >
+//           +1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
+
+// state를 constructor에서 꺼내기
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 default value 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
   render() {
-		// state를 조회할 때는 this.state로 조회합니다.
-    const { number, fixedNumber } = this.state;
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
       <div>
         <h1>{number}</h1>
@@ -51,7 +84,7 @@ class Counter extends Component {
           // onClick을 통해 버튼이 클릭되었을 때, 호출할 함수 지정
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number:number+1 })
+            this.setState({ number:number+1 });
           }}
         >
           +1
